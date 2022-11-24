b60a9fb1e2a6680f5289f93e84fbbd3b3e948b2e
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-11-25 01:26:44 +0700

    client side rendering

--- index.js
+++ index.js
@@ -4,19 +4,51 @@ const root = document.getElementById("root")
 
 linkHome.onclick = function (event) {
   event.preventDefault()
-  root.textContent = "Welcome to Home"
+  const homeScreen = HomeScreen()
+  root.innerHTML = ""
+  root.append(homeScreen)
   history.pushState(null, "", event.target.href)
 }
 
 linkAbout.onclick = function (event) {
   event.preventDefault()
-  root.textContent = "Welcome to About"
+  root.innerHTML = ""
+  const aboutScreen = AboutScreen()
+  root.append(aboutScreen)
   history.pushState(null, "", event.target.href)
 }
 
+function AboutScreen() {
+  const text = document.createElement("p")
+  text.textContent = "Welcome to About"
+
+  return text
+}
+
+function HomeScreen() {
+  const textPreview = document.createElement("p")
+
+  const input = document.createElement("input")
+  input.placeholder = "Enter your name"
+
+  input.oninput = function (event) {
+    textPreview.textContent = event.target.value
+  }
+
+  const div = document.createElement("div")
+  div.append(input)
+  div.append(textPreview)
+
+  return div
+}
+
 if (location.hash === "#about") {
-  root.textContent = "Welcome to About"
+  root.innerHTML = ""
+  const aboutScreen = AboutScreen()
+  root.append(aboutScreen)
 } else {
-  root.textContent = "Welcome to Home"
+  const homeScreen = HomeScreen()
+  root.innerHTML = ""
+  root.append(homeScreen)
 }
 
